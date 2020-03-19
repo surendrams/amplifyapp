3f4145ccfdf46104d45018791e49b9b226ecc97c
Surendranath Kumar <surendranathkumarms@gmail.com>
2020-03-18 17:37:44 -0700

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Trying to check; How AWS Amplify works. Will this help to complete my
+          Sadhana Project.
         </p>
         <a
           className="App-link"
